c83fc18331f42729c2e123361623420c71e13914
kalivm90 <kalivm90@gmail.com>
2022-10-06 22:14:15 -0700

    Finished calculator layout, missing functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,15 @@
 // TODO
+document.addEventListener("DOMContentLoaded", () => {
+    let buttonContainer = document.querySelector(".button-container");
+
+    function makeNumbers() {
+        let types = ["7", "8", "9", "/", "4", "5", "6", "x", "1", "2", "3", "-", ".", "0", "=", "+"]
+        for (let i = 0; i < types.length; i++) {
+            let button = document.createElement("button")
+            button.setAttribute("class", "button")
+            button.textContent = types[i]
+            buttonContainer.appendChild(button)
+        }
+    }
+    makeNumbers()
+})
